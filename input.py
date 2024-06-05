e2b6340557e36a3ba4b210fcdc0b16f816341b84
Ravi Pandit <ravipandit2001.11.02@gmail.com>
2024-06-05 16:06:59 +0545

    first commit

--- input.py
+++ input.py
@@ -3,8 +3,8 @@ print("My name is ",a)
 
 x=input("Enter first number : ")
 y=input("Enter second number : ")
-print(x+y)
-print(int(x)+int(y))
+print("The concatenation will be : ",x+y)
+print("The sum will be : ",int(x)+int(y))
 
 i=int(input("Enter the number : "))
 print("The number is : ",i)
